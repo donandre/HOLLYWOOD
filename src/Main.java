a93eb82bc0d72f0e7a3c348e4b4cfc7e64cfd9c7
donandre <123123>
2015-04-03 11:30:58 +0300

    added table mapping(Film, Actors, Text)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import org.hibernate.Session;
 
 public class Main {
     public static void main(final String[] args){
-        System.out.println("aa");
+        System.out.println("aaa");
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         System.out.println("Is connected = " + session.isConnected());
